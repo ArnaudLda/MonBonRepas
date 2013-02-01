f075dc7543ab642281a1e71e2cdb98774c298416
Laumond Arnaud <arnaud.laumond@gmail.com>
2013-02-01 12:35:26 +0100

    test fatfree
    je push tout

---
+++ app/Controllers/App_controller.php
@@ -0,0 +1,53 @@
+<?php
+class App_controller{
+ 
+ function __construct(){
+  
+ }
+ 
+ function home(){
+    $id=F3::get('PARAMS.id');
+    #récupération de la destination courante
+    $App=new App();
+    $location=$App->locationDetails($id);
+    if(!$location){
+      F3::error('404');
+      return;
+    }
+    F3::set('location',$location);
+    
+    F3::set('coords',Views::instance()->toJson($location,array('lat'=>'lat','lng'=>'lng')));
+
+    
+    $pictures=$App->locationPictures($location->id);
+
+    $json=Views::instance()->toJson($pictures,array('image'=>'src'));
+    F3::set('pictures',$json);
+    //F3::set('location',App::instance()->locationDetails(););
+    
+    
+    $next=$App->getNext($location->id);
+    $prev=$App->getPrev($location->id);
+    
+   
+    $linkNext=$next?$next[0]['id'].'-'.$next[0]['title']:'';
+    $linkPrev=$prev?$prev[0]['id'].'-'.$prev[0]['title']:'';
+    
+    F3::set('next',$linkNext);
+    F3::set('prev',$linkPrev);
+    
+    
+    echo Views::instance()->render('travelr.html');
+ }
+ 
+ 
+  function doc(){
+    echo Views::instance()->render('userref.html');
+  }
+  
+ 
+ function __destruct(){
+
+ } 
+}
+?>
