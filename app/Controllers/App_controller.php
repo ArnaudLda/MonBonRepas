d7743f79a4efea641c02f27363c567644e196bd0
Laumond Arnaud <arnaud.laumond@gmail.com>
2013-02-06 18:32:52 +0100

    test inscription

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -51,6 +51,31 @@ class App_controller{
  }
   function inscription()
  {
+	 $units=array(
+      array('firstname'=>'$_GET["prenom"]','lastname'=>'francois','email'=>'pumir@hetic.net','email_check'=>'pumir@hetic.net'),
+      /*array('firstname'=>'','lastname'=>'francois','email'=>'pumir@hetic.net','email_check'=>'pumir@hetic.net'),
+      array('firstname'=>'pumir','lastname'=>'','email'=>'pumir@hetic.net','email_check'=>'pumir@hetic.net'),
+      array('firstname'=>'','lastname'=>'','email'=>'pumir@hetic.net','email_check'=>'pumir@hetic.net'),
+      array('firstname'=>'pumir','lastname'=>'francois','email'=>'pumir@heticnet','email_check'=>'pumir@heticnet'),
+      array('firstname'=>'pumir','lastname'=>'francois','email'=>'pumir@hetic.ne','email_check'=>'pumir@hetic.net'),
+      array('firstname'=>'pumir','lastname'=>'francois','email'=>'','email_check'=>'pumir@hetic.net'),
+      array('firstname'=>'pumir','lastname'=>'francois','email'=>'pumir@hetic.net','email_check'=>''),
+      array('firstname'=>'','lastname'=>'francois','email'=>'pumir@hetic.net','email_check'=>'pumir.net')*/
+    );
+    $test=new \Test;
+    foreach ($units as $unit) {
+      F3::mock('POST /travel',$unit);
+      $test->expect(
+        !F3::get('errorMsg'),
+        'POST : '.
+        $unit['firstname'].' | '.
+        $unit['lastname'].' | '.
+        $unit['email'].' | '.
+        $unit['email_check'].' => '.
+        F3::stringify(F3::get('errorMsg'))
+        );
+    }
+    F3::set('results',$test->results());
 	echo Views::instance()->render('inscription.html');
  }
    function dashboard()
@@ -68,6 +93,10 @@ class App_controller{
   function gest_repas()
  {
 	echo Views::instance()->render('Gest_Repas.html');
+ }
+   function test()
+ {
+	echo Views::instance()->render('test.html');
  }
  function __destruct(){
 
