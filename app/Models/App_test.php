e38496125e23eb8170892093e2a2fa573921dc28
Laumond Arnaud <arnaud.laumond@gmail.com>
2013-02-09 17:05:16 +0100

    n�ant

---
+++ app/Models/App_test.php
@@ -0,0 +1,66 @@
+<?php
+class App extends Prefab{
+  
+  function __construct(){
+      F3::set('dB',new DB\SQL(
+        'mysql:host='.F3::get('db_host').';port=3306;dbname='.F3::get('db_server'),
+        F3::get('db_user'),
+        F3::get('db_pw')));
+  }
+  
+  function locationDetails($id=null){
+    //return F3::get('dB')->exec('select * from location limit 1');
+    $location=new DB\SQL\Mapper(F3::get('dB'),'location');
+    if(!$id){
+      return $location->load();
+    }
+    return $location->load(array('id=?',$id));
+  }
+  
+    function create($mail){
+    $inscrit=new DB\SQL\Mapper(F3::get('dB'),'inscrit');
+	$inscrit->find(array('mail=?',$mail));
+	$exist=false;
+	foreach($inscrit as $inscrits)
+	{
+		if($inscrits['mail']==$mail)
+		{
+			$exist=true;
+		}
+	}
+	if($exist)
+	{
+		return false;
+	}
+	else
+	{
+		$inscrit->copyFrom('POST');
+		$inscrit->save();
+	}
+  }
+  
+  function locationPictures($idLocation){
+    $pictures=new DB\SQL\Mapper(F3::get('dB'),'pictures');
+    return $pictures->find(array('idLocation=?',$idLocation));
+  }
+  
+  function getNext($id){  
+    return F3::get('dB')->exec("select id, title from location where id =(select min(id) from location where id > ".$id.")");
+  }
+  
+  function getPrev($id){
+    return F3::get('dB')->exec("select id, title from location where id =(select max(id) from location where id < ".$id.")");
+  }
+  
+
+  function __destruct(){
+
+  }
+}
+
+
+
+
+
+
+?>
